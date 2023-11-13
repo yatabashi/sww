43b62ee72870b07b9e8ba09fb949a549f2877dbe
yatabashi <ijatabashi@gmail.com>
2023-11-13 19:04:10 +0900

    強連結成分分解追加

--- sww.py
+++ sww.py
@@ -1,8 +1,22 @@
 """
-ランダムなグラフの生成
-グラフが連結でなければ連結グラフの集合として出力する／連結させる
+makeRandomPages ← 一旦無視？
+
+強連結にする
+
+強連結成分分解の非再帰化
+    https://qiita.com/KowerKoint/items/870ea9ef7a39f3fe4ce3
+
 Page をサーバー内に保管しておく？
     cf. Web.getHypertextFromHyperlinks
+
+ランダムなグラフの生成→数字の重複を避けるために Page の保存の処置の後
+    あたらしく server を作成するなら関係ないのでは？
+"""
+"""
+強連結成分分解：
+    https://manabitimes.jp/math/1250
+    https://hkawabata.github.io/technical-note/note/Algorithm/graph/scc.html
+
 """
 
 from typing import Type, Union
@@ -63,7 +77,7 @@ class Web:
     def __init__(self, server: Server, initialPageId: int):
         self.server: Server = server  # ハイパーテキストを構築するページ群が置かれたサーバ
         self.initialPageId = initialPageId  # 既知として与えられる周回の起点となるページ
-        self.hypertext: dict[int, set[int]] = dict()
+        self.hypertext: dict[int, set[int]] = dict() # ハイパーテキストを隣接リストとして保持する
         self.reconstruct(initialPageId)
 
     # ハイパーテキストを構築する
@@ -86,6 +100,7 @@ class Web:
     
     # ハイパーテキストを初期化して構築する
     def reconstruct(self, initialPageId: int):
+        self.initialPageId = initialPageId
         self.hypertext.clear()
         self.initialise(initialPageId)
 
@@ -145,7 +160,7 @@ class Web:
     # 新たに Page を生成している←もとの Server 内に Page があるならそれを利用すべき？
     # content が消える問題もある
     @classmethod
-    def getHypertextFromHyperlinks(cls, hyperlinks: set[tuple[int, int]]):
+    def makeHypertextFromHyperlinks(cls, hyperlinks: set[tuple[int, int]]):
         # 行列を辞書に変換する
         d: dict[int, set[int]] = {}
 
@@ -240,101 +255,242 @@ class Web:
             choices = self.server.getPage(locationId).destinationIds
             self.randomwalk(random.choice(list(choices)), destinationId, (None if maxStep is None else maxStep-1), path+[str(locationId)])
     
-    def isConnected(self):
-        def getMember(s: set):
+    # ハイパーテキストを強連結成分（Strongly Connected Components）分解する
+    # 有向グラフの強連結成分とは、その部分木であって任意の2頂点間に双方向に有向路がある（＝強連結である）ものを言う
+    # Kosaraju のアルゴリズムに相当する
+    def getSCCs(self, printsDetails: bool = False) -> set[set[int]]:
+        def getMember(s: set) -> any:
             for m in s:
                 return m
-
-        registeredPageIds = self.hypertext.keys()
-        visitedPageIds = set()
-        stack = []
-
-        startPageId = getMember(registeredPageIds)
-        visitedPageIds.add(startPageId)
-        stack.append(startPageId)
-
-        while stack:
-            pageId = stack.pop(-1)
-
-            if self.server.getPage(pageId) is None:
-                continue
-
-            for destinationId in self.server.getPage(pageId).destinationIds:
-                if destinationId not in visitedPageIds:
-                    visitedPageIds.add(destinationId)
-                    stack.append(destinationId)
         
-        return not bool(registeredPageIds - visitedPageIds)
+        # ページをラベリングする
+        hypertext = self.getHypertext()
+        ## ラベリング関数の定義
+        ## 削除されたページもラベリングされる
+        def label(n: int = 0, pageIdToLabel: dict[int, int] = {}, visitedPageIds: set[int] = set()):
+            # 片道のラベリング関数の定義
+            # 始点に戻ってきたら終了し、未周回のページを残しうる
+            def label_oneway(locationId: int, n: int = 0) -> int:
+                # 周回済だった場合（処理済であるか、のちに処理されるので無視）
+                if locationId in visitedPageIds:
+                    return n
+                # リンク先がない場合
+                elif not hypertext.get(locationId):
+                    visitedPageIds.add(locationId) # 周回済にする
+                    pageIdToLabel[locationId] = n  # ラベリングする
+                    n += 1                         # 次のラベルの値をこのページのラベル + 1 にする
+                    return n                       # 次に付けられるべきラベルの値を返す
+                # リンク先がある場合
+                else:
+                    visitedPageIds.add(locationId)
+                    # 各リンク先において片道のラベリングを行う
+                    for destinaionId in hypertext.get(locationId):
+                        n = label_oneway(destinaionId, n) # 次のラベルの値を与え、再帰する
+                    pageIdToLabel[locationId] = n
+                    n += 1
+                    return n
+
+            # ラベリングされていないページがあるならば
+            if leftPageIds := hypertext.keys() - visitedPageIds:
+                # 適当なページから片道のラベリングを行う
+                n = label_oneway(getMember(leftPageIds), n)
+
+                # 再帰的にラベリングを続ける
+                return label(n, pageIdToLabel, visitedPageIds)
+            # すべてラベリングされているならば
+            else:
+                return pageIdToLabel
+        
+        ## ラベリングの実行
+        pageIdTolabel = label()
+        labelToPageId = {label: pageId for (pageId, label) in pageIdTolabel.items()}
+
+        # ハイパーテキストの転置グラフ*を取得する
+        # *すべてのエッジ（リンク）を反転させたものを言う
+        def getTransposeHypertext():
+            hyperlinks = self.getHyperlinks()
+            transpose = {hyperlink[::-1] for hyperlink in hyperlinks}
+            return Web.makeHypertextFromHyperlinks(transpose)
+
+        ## 取得の実行
+        transposeHypertext = getTransposeHypertext()
+
+        # decompose: 辿り直して分解
+        def getComponents(foundComponents: set[frozenset[int]] = set(), visitedPageIds: set[int] = set()) -> set[frozenset[int]]:
+            # 強連結成分を一つ取得する関数の定義
+            # あるページから転置グラフを辿って到達可能な（自身をリンク先としている）ページの集合を返す
+            def getOneComponent(locationId: int) -> frozenset[int]:
+                # 周回済だった場合
+                if locationId in visitedPageIds:
+                    if printsDetails: print(locationId, "is visited; return")
+                    return frozenset()
+                # リンク先がない場合
+                elif not transposeHypertext.get(locationId):
+                    if printsDetails: print(locationId, "has no links; return")
+                    visitedPageIds.add(locationId) # 周回済にする
+                    return frozenset({locationId})
+                # リンク先がある場合
+                else:
+                    if printsDetails: print(locationId, "has link;")
+                    visitedPageIds.add(locationId)
+                    rv = frozenset({locationId})
+                    for destinaionId in transposeHypertext.get(locationId):
+                        rv |= getOneComponent(destinaionId)
+                        if printsDetails: print("component is updated:", rv)
+                    return rv
+
+            # 未周回のページがあるならば
+            if transposeHypertext.keys() - visitedPageIds:
+                nonlocal pageIdTolabel
+                nonlocal labelToPageId
+
+                if printsDetails: print("labelling:", labelToPageId)
+                if printsDetails: print("pageIdWithMaxLabel:", labelToPageId[max(labelToPageId.keys())])
+
+                # ラベルが最大のページから強連結成分を取得する
+                component = getOneComponent(labelToPageId[max(labelToPageId.keys())])
+
+                # 分解された成分に割り当てられたラベリングの削除
+                # 最大のラベルを取得しやすくする
+                pageIdTolabel = {pageId: label for (pageId, label) in pageIdTolabel.items() if pageId not in component}
+                labelToPageId = {label: pageId for (pageId, label) in pageIdTolabel.items()}
+                if printsDetails: print("component:", component)
+
+                # 分解された成分を強連結成分の集合に追加する
+                foundComponents.add(component)
+
+                # 再帰的にラベリングを続ける
+                return getComponents(foundComponents, visitedPageIds)
+            # すべてラベリングされているならば
+            else:
+                return foundComponents
+            
+        return getComponents()
+
+    def isStronglyConnected(self):
+        return len(self.getSCCs())
+
+
+
 
 if __name__ == "__main__":
     """
-        0 
-      ↙︎ ⇅
-  ⇨ 1 → 2 → 8
-    ↓ ↗︎ ↑
-    3 → 4 ← 5 ⊃
+        0   8 ← 10
+      ↙︎ ⇅     ↘︎ ↑
+  ⇨ 1 → 2       9 → 11
+    ↓ ↗︎ ↑   ∩   ↑
+    3 → 4 ← 5 ⇄ 12
       ↖︎ ↓ ↗︎ ↑
-        7   6
+        7  (6)
+
+        0   8 ← 10
+      ↙︎ ⇅     ↘︎ ↑
+  ⇨ 1 → 2       9 → 11
+    ↓ ↗︎ ↑   ∩   ↑
+    3 → 4 ← 5 ⇄ 12
+      ↖︎ ↓ ↗︎ ↑
+        7  (g)
     """
 
-    page0 = Page(0, "zero", {1, 2})
-    page1 = Page(1, "un", {2, 3})
-    page2 = Page(2, "deux", {0, 8})
-    page3 = Page(3, "trois", {2, 4})
-    page4 = Page(4, "quatre", {2, 7})
-    page5 = Page(5, "cinq", {4, 5})
-    page6 = Page(6, "six", {5})
-    page7 = Page(7, "set", {3, 5})
-    page8 = Page(8, "huit", set())
-    server = Server({page0, page1, page2, page3, page4, page5, page6, page7, page8})
+    page0 = Page(0, "a", {1, 2})
+    page1 = Page(1, "b", {2, 3})
+    page2 = Page(2, "c", {0})
+    page3 = Page(3, "d", {2, 4})
+    page4 = Page(4, "e", {2, 7})
+    page5 = Page(5, "f", {4, 5, 12})
+    page6 = Page(6, "g", {5})
+    page7 = Page(7, "h", {3, 5})
+    page8 = Page(8, "i", {9})
+    page9 = Page(9, "j", {10, 11})
+    page10 = Page(10, "k", {8})
+    page11 = Page(11, "l", set())
+    page12 = Page(12, "m", {5, 9})
+    server = Server({page0, page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11, page12})
     web = Web(server, 1)
-    print(web.getSortedHypertext())
-    print(web.isConnected(), "but", server.record.keys() - web.getHypertext().keys(), "are missing")
+    print("hypertext:", web.getSortedHypertext())
+    print(web.isStronglyConnected(), "SCCs")
+    print("\n——————————\n")
+
+    # web.tp(); 
 
     """
-         
-        ↑
-    1 → 2   8
-    ↓ ↗︎ 
-    3 → 4 ← 5 ⊃
+       ~a~  i ← k
+        ↑     ↘︎ ↑
+    b → c       j → l
+    ↓ ↗︎ ↑   ∩   ↑
+    d → e ← f ← m
       ↖︎ ↓ ↗︎ ↑
-        7 → 6
+        h → g
             ↑
-            9
+           (n)
+
+       ~0~  11← 8
+        ↑     ↖︎ ↑
+    7 → 1       10→ 9
+    ↓ ↗︎ ↑   ∩   ↑
+    6 → 5 ← 2 ← 12
+      ↖︎ ↓ ↗︎ ↑
+        4 → 3
+            ↑
+           (n)
     """
 
     server.deletePage(0)
-    page4.deleteLink(2)
-    page2.deleteLink(8)
+    page5.deleteLink(12)
     page7.addLink(6)
-    page9 = Page(9, "dix", {6})
-    server.addPage(page9)
+    page13 = Page(13, "n", {6})
+    server.addPage(page13)
     web.crawl()
-    print(web.getSortedHypertext())
-    print(web.isConnected(), "but", server.record.keys() - web.getHypertext().keys(), "are missing")
+    print("hypertext:", web.getSortedHypertext())
+    print(web.isStronglyConnected(), "SCCs")
+    print("\n——————————\n")
 
     """
-         
+       ~0~
         ↑
-    1 → 2   8
-    ↓ ↗︎ 
-    3 → 4 ← 5 ⊃
-    ↑ ↖︎ ↓ ↗︎ ↑
-    ↑ ← 7 → 6
+        2
+      ↗︎ ↑   ∩
+    3 → 4 ← 5
+      ↖︎ ↓ ↗︎ ↑
+        7 → 6
             ↑
-            9 ⇦
+            13⇦
+
+       ~0~
+        ↑
+        1
+      ↗︎ ↑   ∩
+    2 → 4 ← 5
+      ↖︎ ↓ ↗︎ ↑
+        3 → 6
+            ↑
+            7 ⇦
     """
     
-    web.reconstruct(9)
-    print(web.getSortedHypertext())
-    print(web.isConnected(), "but", server.record.keys() - web.getHypertext().keys(), "are missing")
-
-    hls = web.getHyperlinks()
-    print(hls)
-    hlsdash = Server.getPagesFromHyperlinks(hls)
-    print(Web(Server(hlsdash), 9).getHypertext())
+    web.reconstruct(13)
+    print("hypertext:", web.getSortedHypertext())
+    print(web.isStronglyConnected(), "SCCs")
+    print("\n——————————\n")
 
     # web.explore()
 
-    for _ in range(5):
-        web.randomwalk(9, 3, 9)
+    # for _ in range(5):
+    #     web.randomwalk(9, 3, 9)
+
+    """
+    0 → 1 → 2 → 4
+        ↑ ↙︎
+        3
+    """
+    pagea = Page(20, "", {21})
+    pageb = Page(21, "", {22})
+    pagec = Page(22, "", {23, 24})
+    paged = Page(23, "", {21})
+    pagee = Page(24, "", {})
+    servera = Server({pagea, pageb, pagec, paged, pagee})
+    weba = Web(servera, 20)
+    print("hypertext:", weba.getSortedHypertext())
+    print(web.isStronglyConnected(), "SCCs")
+    print("\n——————————\n")
+
+    
